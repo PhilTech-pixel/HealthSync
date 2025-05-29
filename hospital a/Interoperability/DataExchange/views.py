1b82490bde2c89aefda5a3140fa305647615c069
Philip Njoroge <131545679+PhilTech-pixel@users.noreply.github.com>
2025-05-29 23:44:01 +0300

    Update views.py

--- hospital a/Interoperability/DataExchange/views.py
+++ hospital a/Interoperability/DataExchange/views.py
@@ -10,7 +10,7 @@ from django.dispatch import receiver
 import requests
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, AllowAny  # Use authentication later
+from rest_framework.permissions import IsAuthenticated, AllowAny 
 from .models import MedicalRecord, DataExchangeLog, Hospital
 from .serializers  import MedicalRecordSerializer
 
