0599d33a0deb63fe8e1f85a1e201de6a8b449688
Philip Njoroge <131545679+PhilTech-pixel@users.noreply.github.com>
2025-05-29 23:44:29 +0300

    Update views.py

--- hospital a/Interoperability/DataExchange/views.py
+++ hospital a/Interoperability/DataExchange/views.py
@@ -54,7 +54,7 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.set_password(form.cleaned_data["password"])  # Hash the password
+            user.set_password(form.cleaned_data["password"]) 
             user.save()
 
             # Create UserProfile with selected role
